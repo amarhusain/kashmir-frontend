94b9e27763eca998d1d848c683b4e5fdf8bef1e9
Ratnakar Paswan <ratnakar15799@gmail.com>
2023-03-18 21:21:12 +0530

    second commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kashmir-store'`, () => {
+  it(`should have as title 'kashmir-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kashmir-store');
+    expect(app.title).toEqual('kashmir-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('kashmir-store app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('kashmir-frontend app is running!');
   });
 });
