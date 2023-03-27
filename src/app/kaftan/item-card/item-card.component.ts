102bc5cb70a53db13c5b64475818904f1b2023f7
amarhusain <amar.husain156@gmail.com>
2023-03-27 22:39:37 +0530

    learned ngfor loop

--- src/app/kaftan/item-card/item-card.component.ts
+++ src/app/kaftan/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemCardComponent implements OnInit {
 
+  @Input() item: any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  //  item=  {
+  //   id: 101,
+  //     imgUrl: 'assets/images/kaftan/kaft1.webp',
+  //       name: 'Coral White Printed Georgette Kaftan',
+  //         price: 200
+  // }
 
 }
